f156e357e73d69aab94f0f82b6e4ab38111082a1
Fred Bahr <flbahr@mbari.org>
2018-05-14 12:41:02 -0700

    Add files via upload

--- read_HAB_email_to_post.py
+++ read_HAB_email_to_post.py
@@ -13,24 +13,36 @@ from name_len import name_len
 import matplotlib.pyplot as plt
 import pickle
 #
+""" This is code to read HAB e-mail messages from zimbra and create a pickle file of the data
+"""
+# set up month names for reading
 months=['January','February','March','April','May','June','July','August','September','October','November','December']
 mons=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
 # set up e-mail credentials so we can access e-mails
+# I will NOT post creds.json as that contains both my username and pw
+# This section reads the username and password from a json file to allow access to the e-mail.
 with open('creds.json') as f:
     data=json.load(f)
     username=data['username']
     password=data['password']
+# instantiate the password manager
 password_mgr=urllib2.HTTPPasswordMgrWithDefaultRealm()
 top_level_url='YOUR URL GOES HERE'
-password_mgr.add_password(None,top_level_url,'flbahr',password)
+# add your password to the password manager
+password_mgr.add_password(None,top_level_url,username,password)
+# create a handler to authentic username and password
 handler=urllib2.HTTPBasicAuthHandler(password_mgr)
+# actually login and open the e-mail directory.
 opener=urllib2.build_opener(handler)
 # Now look at my inbox subdirectory HAB_email and get message ID's
 testurl='https:YOUR URL GOES HERE /HAB_email?fmt=json'
+# open the mailbox
 opener.open(testurl)
 urllib2.install_opener(opener)
 test=urllib2.urlopen(testurl)
+# read the messages in the mailbox
 y=test.read()
+# decode the massages
 kk=json.JSONDecoder().decode(y)
 mykeys=kk.keys()
 ck=kk[mykeys[0]]
@@ -42,6 +54,8 @@ ll=np.arange(0,len(letterids))
 #
 # okay load my "dictionary" of plankton names. This could be shoved off to a function.
 # This should probably be moved out of the loop.
+# This creates a dictionary of the plankton names from a simple text file.  Ths was done so
+# it would be easy to edit the names as we come across new names.
 fname="c:/test/plankton_names.txt"
 content=[]
 with open(fname) as f:
@@ -62,6 +76,8 @@ lp=len(content)
 # We need to condense the list a bit for output of data
 # So how do we do this?
 # need to treat Rhizosolenia and Rhizosolenia spp the same
+# the section below removes superfluous information to cut down the number of actual names.
+# These are still unique names for what is observed.
 poutput=[]
 myp=content[:] # make an actual copy so we can manipulate and not change original
 for ip in np.arange(0,lp):
@@ -87,7 +103,9 @@ myp.remove('Acantharians') # just one case of plural so remove
 # each array is lp plankton by n-days?
 #
 # data frame
+# set up an array of station names (we can increase if we get more stations to the north)
 stations=['Santa Cruz','Monterey','Cal Poly','Newport','Stearns','Santa Monica','Scripps']
+# set up some other columns to add to our output beyond the plankton names
 columns=['date','water temp','air temp','water color','dominant']
 # need a set for phyto names
 # let us ignore this for the moment and see if we can load water temp and dates
@@ -104,16 +122,11 @@ for si in np.arange(0,slen):
     HAB_station[stations[si]]=pd.DataFrame(blankdict)
 # we now have a data frame for each sampling site
 # lets see if we can actually load data into it...
-
 counts=np.zeros(7)
 # Start looping through the messages
-#
+# This where the work gets done.
 for jj in ll:
-    #if jj==2:
-    #    pdb.set_trace()
     mid=letterids[jj]
-    # below is set up for testing particular messages
-    #mid='96134'
     # actually grab a particular message
     url='https:YOUR URL GOES HERE ?id='+mid
     opener.open(url)
@@ -123,9 +136,12 @@ for jj in ll:
     # for encoded e-mail we want to do the next few lines
     msg=email.message_from_string(x)
     whom=msg['From']
+    # some code to check that we are actually working
     print '\n'
     print str(jj)+' '+whom+' '+mid
+    # determine if the e-mail needs to be decoded
     myfmt=msg.get_content_type()
+    # get the actual message body (aka the payload)
     mytest=msg.get_payload()
     # check if we try to decode or just use the result
     try:
@@ -133,6 +149,8 @@ for jj in ll:
     except:
         mybody=mytest[:]
     # Do we use x or have replaced this with mybody below?
+    # convert some stuff we don't care about to either nothing or spaces
+    # this will help in parsing the message payload.
     x=x.replace("\r","")
     x=x.replace("\t"," ")
     x=x.replace("=\n"," ")
@@ -151,6 +169,7 @@ for jj in ll:
     mybody=mybody.replace("\xc2","")
     #mybody=mybody.replace("-","") # Doing this makes it fail
     # make an actual copy and not a link to the array
+    # we do this since we want a body that is not split also 
     my=mybody[:]
     #my=x[:] # make a copy?
     my=my.replace(" ","")
@@ -159,19 +178,14 @@ for jj in ll:
     # split the message into words based upon spaces
     # still might have funky characters.
     m=mybody.split()
-    # we can use e-mail access above so the following lines aren't needed.
-    #m=x.split()
-    #fis=[i for i, y in enumerate(m) if (y=="From:")]
-    #whom=m[fis[0]+1]+' '+m[fis[0]+2]
     #
     # Using the name to define the length of the location name to find
     # name is the number of words to look for +1
     name=name_len(whom)
-    #
+    # search for the location in the e-mail
     mis=[i for i, y in enumerate(m) if (y=="LOCATION:")or(y=="Location:")]
     # we have a problem there is no space between DATE:16-Jan-2018 ! how do we find this issue?
     dis=[i for i, y in enumerate(m) if (y=="DATE:")or(y=="Date:")]
-    #pdb.set_trace()
     # we have an issue if DATE: is not in the file
     if len(dis)==0:
         mys1=set(months)
@@ -186,11 +200,11 @@ for jj in ll:
     else:
         nodate=1
     lm=np.arange(0,len(mis))
-    #ld=np.arange(1,len(dis)) # first date is part of the mail header and not the body
     ld=np.arange(0,len(dis)) # we've stripped the header info now
     ln=np.arange(1,name)
     mylocation=[]
     # this is set up to fix some problems with finding locations
+    # some of the messages don't have the location word so thse are to place those messages.
     if whom=='"Carter, Melissa" <mlcarter@ucsd.edu>':
         mylocation.append('Scripps Pier, La Jolla CA')
     if whom=='Melissa Carter <mlcarter@ucsd.edu>':
@@ -218,6 +232,7 @@ for jj in ll:
     # site index now should have a value from 0 to 6
     if len(siteindex) <= 0:
         # we did not get a location so halt and work to fix this
+        # This is since we use location for id of data.
         pdb.set_trace()
     # Now we actually get the date deal with the case where we don't have the word DATE first
     if nodate==0:
@@ -268,19 +283,20 @@ for jj in ll:
     #
     myords=HABdate_to_ordinal(mydates,stations[siteindex[0]])
     # Now to decode the dates and make them into something actually useful
-    #
+    # This loop puts the data into the data frame at the spot we want it.
     for nd in np.arange(0,len(mydates)):
         HAB_station[stations[siteindex[0]]].at[counts[siteindex[0]],'date']=myords[nd]
 #        HAB_station[stations[siteindex[0]]].at[counts[siteindex[0]],'date']=mydates[nd]
         counts[siteindex[0]]=counts[siteindex[0]]+1
     #
-    # dis tells us when we are in the second part of the message
-    # for a multi day sample message
+    # The variable dis (see above) tells us when we are in the second part of the message
+    # for a multi-day sample message
     #
     dgind=[i for i, y in enumerate(m) if (y=="DOMINANT")or(y=="Dominant")]
     # okay there may or may not be a parenthesis (diatoms,dinoflagellates)
     lg=np.arange(0,len(dgind))
     mydom=[]
+    # This next section looks for the dominant phytoplankton group.
     for i in lg:
         # look at second word to check that it is GROUP or Group or group
         wtmp=m[dgind[i]+1]
@@ -310,17 +326,20 @@ for jj in ll:
     import re
     # okay this is returning the wrong number of things....
     from find_plankton_word import find_plankton_word
+    # list of possible abundance words after then name
     abundance=['x','r','p','c','a','none','rare','present','abundant','x:','r:','p:','c:','a:']
+    # list of possible abundance words before the plankton name
     abundbefore=['x:','r:','p:','c:','a:','x','r','p','c','a']
+    # list of words that can follow the keyed on plankton name
     awafter=['sanguinae','spp.','pseudogonyaulax','brown','cf','azicorum','candelabrum','divarcatum','furca','fusus',
              'lineatum','macroceros','penatogonum','tripos','teres','acuminata','caudata','fortii','mitra','brevis',
              'polyedra','polyedrum','cf','gracile','micans','delicatissima','seriata','sanguinea:']
     # look for each plankton word in "dictionary"
     for l in np.arange(0,lp):
+        # test case to find Akashiwo and break 
         #if content[l]=='Akashiwo':
         #    pdb.set_trace()
         wordind=find_plankton_word(my,m,content[l])
-        #pdb.set_trace()
         # is the word index in the first day in the file or the second day?
         # number of words in plankton word
         pword=content[l].split()
@@ -343,6 +362,7 @@ for jj in ll:
     # now we have the location of the plankton word check the word before and after it
     # so what words are we looking for?
     # x,X c,C P,p R,r None Rare Present Common Abundant a,A
+    # Now we are checking for the abundance characters.... Look both before and after the plankton word.
         lw=np.arange(0,len(wordind))
         for ia in lw:
             stuffit=np.nan
@@ -361,6 +381,8 @@ for jj in ll:
                     stuffit=counts[siteindex[0]]-1
             #if ndays > 1:
             #    pdb.set_trace()
+            # some e-mails have both "plain text" and "html" if it has "html" we don't want to count things
+            # twice to break out of the loop.
             if wordind[ia] > htmlstart:
                 break
             wordbefore=m[wordind[ia]-1]
@@ -373,12 +395,14 @@ for jj in ll:
             wordafter=wordafter.lower()
             #if l==5:
             #    pdb.set_trace()
+            # this is clean up so that we can reset and find the index.
             try:
                 del abind
                 del afind
             except:
                 pass
             abind=[i for i,q in enumerate(abundbefore) if q==wordbefore]
+            # commented out logic is to show thoughts that didn't pan out.
             #if len(abind)==0:
             #afind=[i for i,q in enumerate(abundance) if ((q==wordafter)&(q not in abundbefore)) ]
             afind=[i for i,q in enumerate(abundance) if (q==wordafter) ]
@@ -427,7 +451,7 @@ for jj in ll:
     from find_word_val import find_word_val
     [wdindex,wtemp]=find_word_val(my,m,'Water Temperature:')
     wtemp=find_wtemp(wdindex,wtemp,m)
-    # need code to see if we want to stop this
+    # need code to see if we want to stop this (note no ":" here)
     [wdindex,wtemp]=find_word_val(my,m,'Water Temperature')
     wtemp=find_wtemp(wdindex,wtemp,m)
     # some where we need to check if len(wdindex) > 0 to sort between these
@@ -438,11 +462,12 @@ for jj in ll:
     # again need to check if adindex > 0
     [adindex,atemp]=find_word_val(my,m,'Air Temp')
     atemp=find_atemp(adindex,atemp,m)
-    print "My dates"
-    print dis
-    print mydates
-    print "Air Temperature"
-    print atemp
+    # thise are just checks to make sure we are retrieving the correct thing.
+    #print "My dates"
+    #print dis
+    #print mydates
+    #print "Air Temperature"
+    #print atemp
     # Now let's put the air temperature in our data frame
     # Will need to check that stuff doesn't get offset if we have more than one day in a file.
     na=len(atemp)
@@ -462,11 +487,14 @@ for jj in ll:
         if "/" in str(atemp[nd]):
             atemp[nd]=atemp[nd].replace("/",".")
         if stations[siteindex[0]] in 'Santa Cruz':
+            # convert from deg F to deg C for the only station that has value as deg F.
             atemp[nd]=round((float(atemp[nd])-32)*5.0/9.0,1)
         try:
             atemp[nd]=round(float(atemp[nd]),2)
         except:
+            # stop.  We hit a case not covered....
             pdb.set_trace()
+        # fill in our data frame
         HAB_station[stations[siteindex[0]]].at[counts[siteindex[0]]-ndays+nd,'air temp']=atemp[nd]
     #
     # Let's do the same for water temperature
@@ -482,17 +510,19 @@ for jj in ll:
         try:
             wtemp[nd]=round(float(wtemp[nd]),2)
         except:
+            # stop. we hit a case not covered....
             pdb.set_trace()
+        # fill in our data frame
         HAB_station[stations[siteindex[0]]].at[counts[siteindex[0]]-ndays+nd,'water temp']=wtemp[nd]
     ndom=len(mydom)
+    # put the dominant type into the data frame
     for ni in np.arange(0,ndom):
         if mydom[ni]=='even':
             mydom[ni]='even mix'
         if mydom[ni]=='Equal':
             mydom[ni]='even mix'
         HAB_station[stations[siteindex[0]]].at[counts[siteindex[0]]-ndays+ni,'dominant']=mydom[ni]
-    #if jj==0:
-    #    pdb.set_trace()
+    # look for water color/visibility/conditions 
     [vindex1,vis1]=find_word_val(my,m,'Visibility:')
     [vindex2,vis2]=find_word_val(my,m,'Water Visibility:')
     [wcindex,wcond]=find_word_val(my,m,'Water Conditions:')
@@ -515,7 +545,7 @@ for jj in ll:
                         wcond[iw]=wcond[iw]+' '+colortmp[i]
                     
     # how do we know how much beyond we need to search?...
-    print wcond
+    #print wcond
     [wci1,wcolor1]=find_word_val(my,m,'Water Color/Code:')
     wcii=len(wci1)
     if wcii > 0:
@@ -528,7 +558,7 @@ for jj in ll:
                     for i in np.arange(0,len(colortmp)):
                         wcolor1[iw]=wcolor1[iw]+' '+colortmp[i]
     [wci2,wcolor2]=find_word_val(my,m,'Water Color:')
-    print wcolor1
+    #print wcolor1
     if wci2 > 0:
         wcix=len(wci2)
         for iw in np.arange(0,wcix):
@@ -540,16 +570,14 @@ for jj in ll:
                     #pdb.set_trace()
                     for i in np.arange(0,len(colortmp)):
                         wcolor2[iw]=wcolor2[iw]+' '+colortmp[i]
-    print wcolor2
-    #if jj==9:
-    #    pdb.set_trace()    
+    #print wcolor2
     [wci3,wcolor3]=find_word_val(my,m,'COLOR:')
     if len(wci3) > 0:
         [wchl,chl]=find_word_val(my,m,'CHL')
         if len(wchl) > 0:
             for wiii in np.arange(2,(wchl[0]-wci3[0])):
                 wcolor3[0]=wcolor3[0]+' '+m[wci3[0]+wiii]
-    print wcolor3
+    #print wcolor3
     [tindex1,times1]=find_word_val(my,m,'Time:')
     [dindex1,dates1]=find_word_val(my,m,'Date:')
     if len(wcindex) > 0:
@@ -578,13 +606,11 @@ for jj in ll:
             HAB_station[stations[siteindex[0]]].at[counts[siteindex[0]]-ndays+ni,'water color']=wcolor[ni]
     except:
         pass
+# create a pickle file of the dataframe from the e-mails.
+# code is too long so stop and this point and let another piece of code write out to NetCDF.
 f=open('mydatatest.pickle','wb')
 pickle.dump(HAB_station,f)
 f.close()
-#nd=np.arange(0,len(HAB_station['Stearns']['date']))
-#mytimes=[]
-#for i in nd:
-#    mytimes.append(datetime.date.fromordinal(int(HAB_station['Stearns']['date'][i]))) 
 
 
 
